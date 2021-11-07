19e89cc77e111d7c7c1330eab3ac1893d85bc6cf
최재강 <chl8273@naver.com>
2021-11-07 20:05:15 +0900

    change label data type int32 -> int64

--- vgg16.py
+++ vgg16.py
@@ -52,7 +52,7 @@ def read_labeled_tfrecord(example):
     }
     example = tf.io.parse_single_example(example, LABELED_TFREC_FORMAT)
     image = decode_image(example['image'])
-    label = tf.cast(example['class'], tf.int32)
+    label = tf.cast(example['class'], tf.int64)
     return image, label  # returns a dataset of (image, label) pairs
 
 
