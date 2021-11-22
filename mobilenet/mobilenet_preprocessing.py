c5e684fe5aa59317637323c048fcda37cce2d9d9
최재강 <chl8273@naver.com>
2021-11-22 16:49:56 +0900

    Update mobilenet_preprocessing.py

--- mobilenet/mobilenet_preprocessing.py
+++ mobilenet/mobilenet_preprocessing.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""ImageNet preprocessing for ResNet."""
+"""ImageNet preprocessing for MobileNet."""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
