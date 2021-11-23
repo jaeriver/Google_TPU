7eb017386a8b0b05d41231b6e30e26665bbcae21
최재강 <chl8273@naver.com>
2021-11-23 15:45:12 +0900

    Update inference.py

--- mobilenet/inference.py
+++ mobilenet/inference.py
@@ -4,7 +4,7 @@ import numpy as np
 import tensorflow as tf
 import pandas as pd
 from matplotlib import pyplot as plt
-from tensorflow.keras.models import load_model
+from tensorflow.keras.models import load
 import sys
 import argparse
 import json
@@ -117,7 +117,7 @@ def tpu_inference(model_type, batch_size):
 #   load_locally = tf.saved_model.LoadOptions(experimental_io_device='/job:localhost')
     load_start = time.time()
     with tpu_strategy.scope():
-        model_tpu = load_model(tpu_saved_model_name)
+        model_tpu = load(tpu_saved_model_name)
         load_time = time.time() - load_start
         counter = 0
     #     resname = list(model_tpu.fetch_tensors.keys())[0]
