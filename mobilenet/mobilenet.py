b9c4be549603f149297126e0cd674af2a07be549
최재강 <chl8273@naver.com>
2021-11-23 15:12:45 +0900

    Update mobilenet.py

--- mobilenet/mobilenet.py
+++ mobilenet/mobilenet.py
@@ -199,7 +199,7 @@ def main(unused_argv):
       metrics.append(sparse_top_k_categorical_accuracy)
 
     model.compile(
-        optimizer=Adam(learning_rate=0.001),
+        optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
         loss='sparse_categorical_crossentropy',
         metrics=metrics)
 
